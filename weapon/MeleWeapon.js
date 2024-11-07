704d3dc8c2fbaa0d61e813c52344f9089c5c4cd0
Fedor_Semochkin <221800ss@gmail.com>
2024-11-07 14:03:28 +0700

    refactor animations mele weapons

--- weapon/MeleWeapon.js
+++ weapon/MeleWeapon.js
@@ -9,33 +9,79 @@ export class MeleeWeapon extends BaseWeapon {
         this.attackCooldown = 0.8;
         this.damage = 10
 
+        //state atack
+        this.isReturning = false
         this.isAttacking = false
-        this.attackDuration = 0.3
-        this.attackTimer = 0
-        this.swingAngle = Math.PI / 2 
+        this.attackSpeed = 300
+        this.attackDirection = {x: 0, y: 0}
+        this.startPosition = {x: 0, y: 0}
+    }
+    update(deltaTime){
+        if(!this.isAttacking && !this.isReturning){
+            this.updatePosition()
+        }
+        this.rotateTowardsTarget()
+        this.handleAttack(deltaTime)
     }
     handleAttack(deltaTime){
         if(this.isAttacking){
-            this.attackTimer += deltaTime;
-            const progress = this.attackTimer / this.attackDuration;
-            this.angle = this.baseAngle + this.swingAngle * Math.sin(progress * Math.PI);
-    
-            if(this.attackTimer >= this.attackDuration){
-                this.isAttacking = false;
-                this.currentCooldown = this.attackCooldown;
-                this.attackTimer = 0;
+            // move weapon to target
+            this.position.x += this.attackDirection.x * this.attackSpeed * deltaTime
+            this.position.y += this.attackDirection.y * this.attackSpeed * deltaTime
+
+            const distanceTraveled = this.calculateDistance(this.startPosition, this.position)
+            if(distanceTraveled >= this.attackRange){
+                this.isReturning = false
+                this.isAttacking = false
             }
-            this.checkEnemyHit();
-        } else if (this.currentCooldown <= 0){
-            const target = this.findTarget();
+            this.checkEnemyHit()
+        } else if (this.isReturning){
+            //back weapon to player
+            const directionBack = {
+                x: this.startPosition.x - this.position.x,
+                y: this.startPosition.y - this.position.y
+            }
+            const distanceBack = Math.hypot(directionBack.x, directionBack.y)
+            const normalizedBack = {
+                x: directionBack.x / distanceBack,
+                y: directionBack.y / distanceBack
+            }
+            this.position.x += normalizedBack.x * this.attackSpeed * deltaTime
+            this.position.y += normalizedBack.y * this.attackSpeed * deltaTime
+
+            if(distanceBack <= 5){
+                this.isReturning = false
+                this.currentCooldown = this.attackCooldown
+                // back to start position weapon
+                this.position.x = this.startPosition.x
+                this.position.y = this.startPosition.y
+            }
+        } else if (this.currentCooldown <= 0) {
+            const target = this.findTarget()
             if(target){
-                const distance = this.calculateDistance(this.position, target.position);
-                if(distance <= this.attackRange){
-                    this.isAttacking = true;
+                const distanceToTarget = this.calculateDistance(this.position, target.position)
+                // check target distance
+                if(distanceToTarget <= this.attackRange * 2){
+                    this.isAttacking = true
+                    this.startPosition = {x: this.position.x, y: this.position.y}
+
+                    // calculate direction to target
+                    const dx = target.position.x - this.position.x
+                    const dy = target.position.y - this.position.y
+                    const distance = Math.hypot(dx, dy)
+                    this.attackDirection = {
+                        x: dx / distance,
+                        y: dy / distance
+                    }
+                    // move weapon to target
+                    this.angle = this.calculateRotationToTarget(
+                        target.position.x + target.width / 2,
+                        target.position.y + target.height / 2
+                    )
                 }
             }
         } else {
-            this.currentCooldown -= deltaTime;
+            this.currentCooldown -= deltaTime
         }
     }
     
@@ -43,7 +89,7 @@ export class MeleeWeapon extends BaseWeapon {
     checkEnemyHit(){
         this.game.enemies.forEach(enemy => {
             const distance = this.calculateDistance(this.position, enemy.position)
-            if(distance < this.attackRange){
+            if(distance < enemy.width / 2){
                 if(!enemy.isHit){
                     enemy.takeDamage(this.damage)
                     enemy.isHit = true

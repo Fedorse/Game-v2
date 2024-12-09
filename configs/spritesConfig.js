47bee0d347995528a3368c057eb9e6dc76caadfb
Fedor_Semochkin <221800ss@gmail.com>
2024-12-09 16:38:29 +0700

    added sprites configs

---
+++ configs/spritesConfig.js
@@ -0,0 +1,59 @@
+export const SPRITES_CONFIG = [
+  // characters
+  { name: 'playerIdle', src: '../public/assets/characters/Vinny_idle.png' },
+  { name: 'playerWalk', src: '../public/assets/characters/Vinny_walk.png' },
+  { name: 'IdleHunter', src: '../public/assets/characters/IdleHunter.png' },
+  { name: 'RunHunter', src: '../public/assets/characters/RunHunter.png' },
+
+  //enemies
+  { name: 'piglet', src: '../public/assets/enemy/PIglet_walk.png' },
+  { name: 'skeleton', src: '../public/assets/enemy/skeleton.png' },
+  { name: 'mushroom', src: '../public/assets/enemy/mushroom.png' },
+
+  // weapons
+  { name: 'AK', src: '../public/assets/weapons/machineGun.png' },
+  { name: 'shootGun', src: '../public/assets/weapons/shotGun.png' },
+  { name: 'hamer', src: '../public/assets/weapons/hammer.png' },
+  { name: 'mace', src: '../public/assets/weapons/mace.png' },
+  { name: 'sword', src: '../public/assets/weapons/sword.png' },
+  { name: 'bow', src: '../public/assets/weapons/bow.png' },
+
+  // projectiles
+  { name: 'arrow', src: '../public/assets/projectiles/arrow.png' },
+  { name: 'gunShoot', src: '../public/assets/projectiles/shotGunBullet.png' },
+  {
+    name: 'pistolShoot',
+    src: '../public/assets/projectiles/pistolBullet.png',
+  },
+
+  // enviroment maps
+  { name: 'experience', src: '../public/assets/map/expOrb.png' },
+  { name: 'atlas', src: '../public/assets/map/atlas.png' },
+  { name: 'stone', src: '../public/assets/map/stone.png' },
+  { name: 'tree', src: '../public/assets/map/tree.png' },
+  { name: 'chest', src: '../public/assets/map/chest.png' },
+
+  // GUI
+  // buttons
+  { name: 'btnHover', src: '../public/assets/GUI/buttons/buttonHover.png' },
+  { name: 'btnNormal', src: '../public/assets/GUI/buttons/buttonNormal.png' },
+  {
+    name: 'btnPressed',
+    src: '../public/assets/GUI/buttons/buttonPressed.png',
+  },
+  {
+    name: 'settingsBtn',
+    src: '../public/assets/GUI/buttons/pauseBtnInGame.png',
+  },
+  { name: 'checked', src: '../public/assets/GUI/buttons/checked.png' },
+  { name: 'unchecked', src: '../public/assets/GUI/buttons/unChecked.png' },
+  //icons
+  { name: 'hunterIcon', src: '../public/assets/GUI/icons/hunterIcon.png' },
+  { name: 'warriorIcon', src: '../public/assets/GUI/icons/warriorIcon.png' },
+  // bg
+  { name: 'menu', src: '../public/assets/GUI/menu.png' },
+  { name: 'heroBg', src: '../public/assets/GUI/heroBg.png' },
+  { name: 'bgStats', src: '../public/assets/GUI/heroStats.png' },
+  //other
+  { name: 'card', src: '../public/assets/GUI/cardAugment.png' },
+];
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { Game } from './core/Game.js';
 import { ResourceManager } from './managers/ResourceManager.js';
 import { AUDIO_CONFIG } from './configs/audioConfig.js';
+import { SPRITES_CONFIG } from './configs/spritesConfig.js';
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
@@ -9,55 +10,8 @@ canvas.height = innerHeight;
 
 const resourceManager = new ResourceManager();
 
-const imagesLoad = [
-  { name: 'playerIdle', src: '../public/assets/player/Vinny_idle.png' },
-  { name: 'playerWalk', src: '../public/assets/player/Vinny_walk.png' },
-  { name: 'piglet', src: '../public/assets/enemy/PIglet_walk.png' },
-  { name: 'tileset', src: '../public/assets/tileset.png' },
-  { name: 'projectile', src: '../public/assets/projectiles/projectile.png' },
-  { name: 'experience', src: '../public/assets/ExpOrb.png' },
-  { name: 'mele', src: '../public/assets/mele.png' },
-  { name: 'skeleton', src: '../public/assets/enemy/skeleton.png' },
-  { name: 'mushroom', src: '../public/assets/enemy/mushroom.png' },
-  { name: 'sword', src: '../public/assets/sword.png' },
-  { name: 'bow', src: '../public/assets/Bow02.png' },
-  { name: 'arrow', src: '../public/assets/projectiles/Arrow01.png' },
-  { name: 'hamer', src: '../public/assets/Hammer01.png' },
-  { name: 'mace', src: '../public/assets/Mace01.png' },
-  {
-    name: 'pistolShoot',
-    src: '../public/assets/projectiles/PistolAmmoBig.png',
-  },
-  { name: 'gunShoot', src: '../public/assets/projectiles/ShotgunShellBig.png' },
-  { name: 'AK', src: '../public/assets/AK47.png' },
-  { name: 'shootGun', src: '../public/assets/SawedOffShotgun.png' },
-  { name: 'atlas', src: '../public/assets/Atlas.png' },
-  { name: 'stone', src: '../public/assets/Stone.png' },
-  { name: 'tree', src: '../public/assets/tree.png' },
-  { name: 'chest', src: '../public/assets/chest.png' },
-  { name: 'IdleHunter', src: '../public/assets/IdleHunter.png' },
-  { name: 'RunHunter', src: '../public/assets/RunHunter.png' },
-  { name: 'btnHover', src: '../public/assets/GUI/buttons/button_hover.png' },
-  { name: 'btnNormal', src: '../public/assets/GUI/buttons/button_normal.png' },
-  {
-    name: 'btnPressed',
-    src: '../public/assets/GUI/buttons/button_pressed.png',
-  },
-  { name: 'menu', src: '../public/assets/GUI/menu.png' },
-  { name: 'heroBg', src: '../public/assets/GUI/heroBg.png' },
-  { name: 'hunterIcon', src: '../public/assets/GUI/hunterIcon.png' },
-  { name: 'warriorIcon', src: '../public/assets/GUI/warriorIcon.png' },
-  { name: 'bgStats', src: '../public/assets/GUI/heroStats.png' },
-  { name: 'healthBar', src: '../public/assets/GUI/helathBar.png' },
-  { name: 'card', src: '../public/assets/GUI/cardLevel.png' },
-
-  { name: 'checked', src: '../public/assets/GUI/checked.png' },
-  { name: 'unchecked', src: '../public/assets/GUI/unChecked.png' },
-  { name: 'settingsBtn', src: '../public/assets/GUI/settingsMapBtn.png' },
-];
-
 resourceManager
-  .loadImages(imagesLoad)
+  .loadImages(SPRITES_CONFIG)
   .then(() => resourceManager.loadAudios(AUDIO_CONFIG))
   .then(() => {
     const game = new Game(canvas, context, resourceManager);
